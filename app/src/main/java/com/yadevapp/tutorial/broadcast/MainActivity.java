c620525aab5de2a10d59cc2febe77cf1e278a188
abdoulaye yatera <yatera.a@outlook.com>
2018-09-14 22:19:05 +0200

    chore: first commit

--- app/src/main/java/com/yadevapp/tutorial/broadcast/MainActivity.java
+++ app/src/main/java/com/yadevapp/tutorial/broadcast/MainActivity.java
@@ -10,25 +10,10 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = getClass().getSimpleName();
-    private EditText mEditText;
-    private Button mSendBroadcastButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mEditText = findViewById(R.id.activity_main_edittext);
-        mSendBroadcastButton = findViewById(R.id.activity_main_button);
-
-        mSendBroadcastButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: ");
-                String textToSend = mEditText.getText().toString();
-                Intent broadcastIntent = new Intent("com.yadevapp.tutorial.broadcast.intent.action.send");
-                broadcastIntent.putExtra("text", textToSend);
-                sendBroadcast(broadcastIntent);
-            }
-        });
     }
 }
